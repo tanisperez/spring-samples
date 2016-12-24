194663e6b70f41d71f8c87dd8d5473700bd16960
tanis <tanisperez@gmail.com>
2016-12-24 16:58:10 +0100

    Project creation

---
+++ spring-samples/spring-beans/src/main/java/com/tanis/spring/samples/beans/App.java
@@ -0,0 +1,13 @@
+package com.tanis.spring.samples.beans;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
